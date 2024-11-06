7a4db3ce098a8861ad3a7d2c9086c316bbc13f8a
Matthew Semcesen <semcesenm25@mail.wlu.edu>
2024-11-06 16:46:22 -0500

    beginning of intro page

--- Introduction_Page.R
+++ Introduction_Page.R
@@ -1,3 +1,22 @@
 #Introduction Page
 
 library(tidyverse)
+library(shiny)
+library(shinyjs)
+library(ggplot2)
+library(shinyWidgets)
+library(leaflet)
+
+
+navbarPage("Climate Change's Effect on Agricultural Production",
+           
+           
+           # Introduction Tab 
+           
+           tabPanal("Introduction",
+                    h3("Climate Change's Impact on Agriculture across different countries."),
+                    p("Explore how climate change affects agricultural production across different countries."),
+                    leafletOutput("Introduction_Map", height = "800px"),
+                    p("Hover over to view the country name, its popular crop, and its agriculture contribution to GDP")
+           
+           )
