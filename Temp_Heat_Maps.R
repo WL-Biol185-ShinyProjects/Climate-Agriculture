5da7a1df69682c8f209b55ab5071a8fc6c9513e1
Theo Goodchild-Michelman <goodchild-michelmt25@mail.wlu.edu>
2024-11-13 16:00:23 -0500

    UI code draft for temperature heat map(can also use for yield heat maps

--- Temp_Heat_Maps.R
+++ Temp_Heat_Maps.R
@@ -1,3 +1,39 @@
-#Temperature Heat Maps
+#ui.R code for temp changes
 
-library(tidyverse)
+library(shiny)
+library(leaflet)
+
+ui <- fluidPage(
+  
+  titlePanel("Global Climate Change Map"),
+  
+  sidebarLayout(
+    sidebarPanel(
+      
+      helpText(" Visualizing Global Temperature Changes")
+    )
+  ),
+  
+  
+  mainPanel(
+    leafletOutput("map", height = "1000px")
+    
+    
+  )
+)
+
+
+
+  #server.R code
+  
+
+
+  
+  
+  
+
+
+
+
+
+
