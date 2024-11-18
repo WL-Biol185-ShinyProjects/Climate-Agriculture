9575cd6093bdba126ecae4207789ae733619b39b
My Name <charnecoc25@mail.wlu.edu>
2024-11-18 16:08:42 -0500

    Layout for UI code

--- Crop_Heat_Maps.R
+++ Crop_Heat_Maps.R
@@ -7,4 +7,11 @@ library(geojsonio)
 library(dplyr)
 
 
+# #Mock Code for UI once chloropleth is finalized
+# Crop_Heat_Map_ui <- fluidPage("Climate + Agriculture Research",
+#                         navbarMenu("Crop Yield Cloropleth"),
+#                              tabPanel("Historic Crop Yield Data by Country"),
+#                              setBackgroundColor("floralwhite"),
+#                              mainPanel(p("placeholder description of map")),
+#                              leafletOutPut("MAPNAME", height = ("80vh")))
 
