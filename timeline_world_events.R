9d3139c46f5c20c5de6e04982bc1c1dcb6b2bb03
Matthew Semcesen <semcesenm25@mail.wlu.edu>
2024-11-20 16:42:24 -0500

    still working and figuring this out

--- timeline_world_events.R
+++ timeline_world_events.R
@@ -1,1 +1,46 @@
 #TIMELINE OF WORLD EVENTS 
+
+library(shiny) 
+library(ggplot2)
+
+worldevents_ui <- fluidPage(
+  titlePanel("Major World Events Affect on Agriculture"), 
+  
+  sidebarLayout(
+    sidebarPanel(
+      selectInput(
+        inputId = "World Event",
+        label = "Select a World Event",
+        choices = c(
+          "1991 Soviet Union Dissolution",
+          "1997 Asian Financial Crisis",
+          "2004 Indian Ocean Tsunami", 
+          "2008 Global Financial Crisis", 
+          "2011 Arab Spring", 
+          "2014 Ebola Outbreak in West Africa",
+          "2020 COVID-19 Pandemic", 
+          "2022 War in Ukraine"
+        ), 
+        
+        selected = "2020 COVID-19 Pandemic"
+      )
+    ),
+    
+    
+    #displays scatterplot 
+    mainPanel(
+      plotOutput("Yield_Plot")
+    )
+  )
+  
+)
+
+
+#server
+
+worldevents_server <- function(input, output, session){
+  
+  output$Yield_Plot <- renderPlot ({
+    
+  })
+}
