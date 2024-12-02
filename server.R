7e6d4d17f34fc5e5ee583261fe9e5871e9ebd2ef
Matthew Semcesen <semcesenm25@mail.wlu.edu>
2024-12-02 15:10:52 -0500

    deletion of placeholder

--- server.R
+++ server.R
@@ -145,21 +145,3 @@ server <- function(input, output, session) {
     
 }
 
-# Define server logic required to draw a histogram
-#function(input, output) {
-  
-  #loading a test dataset
-  #data <- read.csv("fully_afgan_cambo_cleaned.csv")
-  
-  #output$distPlot <- renderPlot({
-    
-    # generate bins based on input$bins from ui.R 
-    #x  <-  data$Value 
-    #bins <- seq(min(x), max(x), length.out = input$bins + 1)
-    
-    # draw the histogram with the specified number of bins
-    #hist(x, breaks = bins, col = 'darkgray', border = 'white')
-    
-  #})
-  
-#}
