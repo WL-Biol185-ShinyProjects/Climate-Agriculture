570eeea6989eda53f64ed75baa3f9a645837a5b3
Matthew Semcesen <semcesenm25@mail.wlu.edu>
2024-12-10 20:20:29 -0500

    no changes just need to committ

--- server.R
+++ server.R
@@ -1,31 +1,31 @@
 library(shiny)
-library(leaflet) 
+library(leaflet)
 library(geojsonio)
 library(dplyr)
 library(ggplot2)
 library(tidyverse)
 library(tidyr)
-library(sf)
+# library(sf)
 library(countrycode)
 
 countries_data <- readRDS("Countries_data_FailedStates_Islands/combined_countries_data.rds")
 geo <- geojson_read("countries.geo.json", what = "sp")
 
 most_produced_crop <- countries_data %>%
-  
-   
+
+
   filter(Year == 2022, Unit == "t") %>% #filtering finding the most crops produced in tons in 2022
   group_by(Area) %>%  #grouping by country
   filter(Value == max(Value, na.rm = TRUE)) %>%
   ungroup() %>%  #remove the grouping
   select(
-    Area, 
-    Most_Produced_Crop = Item, 
+    Area,
+    Most_Produced_Crop = Item,
     Max_Production = Value
   )
 
 
-#joining geojson with country data 
+#joining geojson with country data
 geo@data <-left_join(geo@data, most_produced_crop, by = c("name" = "Area"))
 
 #reading and processing efficiency data call
@@ -37,7 +37,7 @@ crop_data <- readRDS("efficiency_data /combined_efficiency_data.rds")
 
 #Temp Heat Map Data Modification
 temp_merged <- reactive({
-  
+
   fixed <- read.csv("fully_temp_data_cleaned.csv")
 
   temp.data <- fixed %>%
@@ -48,11 +48,11 @@ temp_merged <- reactive({
       values_to = "Celsius"
     ) %>%
     mutate(Year = as.numeric(Year))
-  
+
   # json file
   world <- st_read("countries.geo.json") %>%
     rename(ISO3 = id)
-  
+
   #  merge by left join
   world %>%
     left_join(temp.data, by = "ISO3")
@@ -61,28 +61,28 @@ temp_merged <- reactive({
 
 #Crop Yield Map data modification
 crop_merged <- reactive({
-  
-  #Filtering and joining data so that we can see the total yield of each country in each year for all crops   
-  yield_table <- crop_data %>% 
+
+  #Filtering and joining data so that we can see the total yield of each country in each year for all crops
+  yield_table <- crop_data %>%
     group_by(Area, Year) %>%
     summarise(Total_Yield = sum(Value, na.rm = TRUE))
-  
-  
-  yield_table$Area <- recode(yield_table$Area, 
-                             "Czechoslovakia" = "Czech Republic", 
+
+
+  yield_table$Area <- recode(yield_table$Area,
+                             "Czechoslovakia" = "Czech Republic",
                              "Serbia and Montenegro" = "Serbia")
-  
+
   yield_table$Country_Code <- countrycode(yield_table$Area, origin = "country.name", destination = "iso3c")
-  
+
   #Taking out any non matches
   crop_data <- yield_table %>%
     filter(!is.na(Country_Code)) %>%
     mutate(Year = as.numeric(Year))
-  
-  
+
+
   yield_geo <- st_read("countries.geo.json") %>%
     rename(Country_Code = id)
-  
+
   yield_geo %>%
     left_join(crop_data, "Country_Code")
 })
@@ -90,50 +90,50 @@ crop_merged <- reactive({
 
 #start of server outputs
 server <- function(input, output, session) {
- 
+
   #introduction page
-   #input introduction map 
+   #input introduction map
   output$Intro_Map <- renderLeaflet({
     pal <- colorBin("Blues", domain = geo@data$Max_Production, na.color = "black")
-    
-    leaflet(geo) %>% 
-      addTiles() %>% 
+
+    leaflet(geo) %>%
+      addTiles() %>%
       addPolygons(
         fillColor = ~pal(Max_Production),
         color = "white",
-        weight = 1, 
-        fillOpacity = 0.7, 
+        weight = 1,
+        fillOpacity = 0.7,
         highlight = highlightOptions(
-          weight = 2, 
-          color = "#666", 
-          fillOpacity = 0.9, 
+          weight = 2,
+          color = "#666",
+          fillOpacity = 0.9,
           bringToFront = TRUE
-        ), 
-        
-        #adding the labels 
+        ),
+
+        #adding the labels
         label = ~paste(
           "Country: ", name,",",
-          "Most Produced Crop: ", Most_Produced_Crop,",", 
+          "Most Produced Crop: ", Most_Produced_Crop,",",
           "Total Production: ", Max_Production, " Tons"
         ),
-        
-       #styling the labels 
+
+       #styling the labels
          labelOptions = labelOptions(
-          style = list("font-weight" = "normal", padding = "3px 8px"), 
-          textsize = "13px", 
+          style = list("font-weight" = "normal", padding = "3px 8px"),
+          textsize = "13px",
           direction = "auto"
-          
-          
+
+
         )
-        
+
       )
-    
+
   })
 
-#WORLD EVENTS AFFECT ON AGRICULTURE 
+#WORLD EVENTS AFFECT ON AGRICULTURE
 
   output$Yield_Plot <- renderPlot ({
-  
+
   #picking years and the country of focus for the event
   details_for_events <- list(
     "Great Flood of 1993 (Mississippi River)" = list(year = 1995, country = "United States of America"),
@@ -144,48 +144,48 @@ server <- function(input, output, session) {
     "Russia Annexation of Crimea (2014)" = list(year = 2016, country = "Ukraine"),
     "Ebola Outbreak in West Africa (2014)" = list(year = 2016, country = "Sierra Leone")
   )
-  
-  #getting the event from the UI   
+
+  #getting the event from the UI
     selected_event <- input$World_Event
-    
+
     event <- details_for_events[[selected_event]]
-    event_year <- event$year    #the year that goes with the event 
-    event_country <- event$country #the country that goes with the event 
-    
+    event_year <- event$year    #the year that goes with the event
+    event_country <- event$country #the country that goes with the event
+
   #filtering the data
-    data_for_event_tab <- countries_data %>% 
+    data_for_event_tab <- countries_data %>%
       filter(
         Year >= (event_year - 4) & Year <= event_year,
         Area %in% event_country, #searched this up - %in% checks if something belongs to a vector or list - so it is checking if the countries in event_country is in the column AREA
-        Unit == "t" ) %>%   #only data unit is in tons 
-      
-      group_by(Year, Area) %>% 
+        Unit == "t" ) %>%   #only data unit is in tons
+
+      group_by(Year, Area) %>%
       summarize(Total_Production = sum(Value, na.rm = TRUE)) %>%
       ungroup()
-    
-    #create the actual plot 
+
+    #create the actual plot
     plot(
       data_for_event_tab$Year, #xaxis - years
       data_for_event_tab$Total_Production, #yaxis - total production
-      type = "b", 
-      col = "red", #line and point color 
+      type = "b",
+      col = "red", #line and point color
       xlab = "Year", #label for the x axis
-      ylab = "Total Crop Production (tons)", #label for y-axis 
-      main = paste("Agriculture Production During", selected_event) #title 
+      ylab = "Total Crop Production (tons)", #label for y-axis
+      main = paste("Agriculture Production During", selected_event) #title
     )
-    
+
   })
-  
-  
-  
+
+
+
 #Code for Farming efficiency tab
   #Altering Input 2 based on input 1
   observeEvent(input$selectedCountry, {
-    
+
     filtered_efficiencydata <- efficiencydata %>%
       filter(Area == input$selectedCountry)
-    
-    
+
+
     updateSelectInput(session, "selectedProduct",
                       choices = c(unique(filtered_efficiencydata$Item)),
     )
@@ -194,61 +194,61 @@ server <- function(input, output, session) {
   output$EfficiencyvsTime <- renderPlot({
     efficiencydata %>%
       filter(Area %in% input$selectedCountry & Item %in% input$selectedProduct) %>%
-      ggplot(aes(Year, Value)) + 
+      ggplot(aes(Year, Value)) +
       geom_line(color = 'blue', size = 2) +
       labs(title = paste("Efficiency vs Time for", input$selectedCountry, "and", paste(input$selectedProduct)),
            x = "Production Years",
            y = "Efficiency (kg/ha)")
   })
-  
+
   #farming efficiency plot variance bar graph
   output$PercentChangevsProduct <- renderPlot({
-    percent_change_data <- efficiencydata %>%                                     
+    percent_change_data <- efficiencydata %>%
       filter(Area %in% input$selectedCountry) %>%
       group_by(Item) %>%
-      #Adding new column for percent change 
-      mutate(First = head(Value, 1),                  
-             BaselineChange =                       
+      #Adding new column for percent change
+      mutate(First = head(Value, 1),
+             BaselineChange =
                case_when(Value != First ~ (Value - First) * 100 / First,
-                         TRUE ~ 1 * NA)) %>%            
+                         TRUE ~ 1 * NA)) %>%
       select(Year, Area, Item, Value, BaselineChange)
-    
+
     #Adding new column to make table show above or below the 1990 value
     percent_change_data$belowabove <- ifelse(percent_change_data$BaselineChange < 0, "below", "above")
-    
+
     #Continued modification
     percent_change_data %>%
       filter(Year == input$Years) %>%
       arrange(BaselineChange, by_group = TRUE) %>%
       #ggplot for processed data
-      ggplot(aes(x=Item, y=BaselineChange, label=BaselineChange)) + 
+      ggplot(aes(x=Item, y=BaselineChange, label=BaselineChange)) +
       geom_bar(stat='identity', aes(fill=belowabove), width=.5)  +
-      scale_fill_manual(name="Efficiency", 
-                        labels = c("Above Average", "Below Average"), 
+      scale_fill_manual(name="Efficiency",
+                        labels = c("Above Average", "Below Average"),
                         values = c("above"="#00ba38", "below"="#f8766d")) +
-      labs(subtitle="Percent change from earliest production value of a given product based on selected year", 
+      labs(subtitle="Percent change from earliest production value of a given product based on selected year",
            title= paste("Diverging Bar Graph for", input$selectedCountry, "in", input$Years),
            y = "Percent Change from Baseline",
-           x = paste("Products offered by", input$selectedCountry)) + 
+           x = paste("Products offered by", input$selectedCountry)) +
       #flipping x and y axis
       coord_flip()
-    
+
   })
-  
-  
+
+
 #Temp variance heat map
   output$temperature_map <- renderLeaflet({
     # Filter data for selected year
-    year_data <- temp_merged() %>% 
+    year_data <- temp_merged() %>%
       filter(Year == input$selected_year)
-    
+
     # color p
     pal <- colorNumeric(
-      palette = input$color_palette, 
+      palette = input$color_palette,
       domain = year_data$Celsius,
       na.color = "transparent"
     )
-    
+
     # make leaflet map + create sick hover pop up
     leaflet(year_data) %>%
       addTiles() %>%
@@ -272,21 +272,21 @@ server <- function(input, output, session) {
         opacity = 1
       )
   })
-  
+
 #Crop Yield Heat Map
   output$yield_map <- renderLeaflet({
-    
+
     # Filter data for selected year
     yield_data <- crop_merged() %>%
       filter(Year == input$selected_year)
-    
+
     # color p
     pal <- colorNumeric(
-      palette = input$color_palette, 
+      palette = input$color_palette,
       domain = yield_data$Total_Yield,
       na.color = "transparent"
     )
-    
+
     # make leaflet map + create sick hover pop up
     leaflet(yield_data) %>%
       addTiles() %>%
@@ -310,9 +310,9 @@ server <- function(input, output, session) {
         opacity = 1
       )
   })
-  
-  
-    
+
+
+
 }
 
 
--- ui.R
+++ ui.R
@@ -5,7 +5,7 @@ library(leaflet)
 library(dplyr)
 library(shinythemes)
 library(base)
-library(sf)
+# library(sf)
 
 #File Designations
 countryfile <- "Countries_data_FailedStates_Islands/combined_countries_data.rds"
@@ -139,22 +139,22 @@ ui <- fluidPage(theme = shinytheme("cosmo"),
               tabsetPanel(
                 #Line Plot Panel
                 tabPanel("Efficiency Vs Product Graph",
-              
+
               p("The plot below depicts the efficiency by which a given country produced a product over the time period 1990 to 2022 (in Kilograms per Hectare). Some products were not produced over that entire time range, and for those the x-axis is adjusted to show this.",
                 style = "font-size: 20px; text-align: center; margin-botton: 20px;"
-              ), 
-              
+              ),
+
               plotOutput("EfficiencyvsTime", width = 800, height = 600),
-              
+
               p("It is apparent that some crops have decreased in production over the time period, which could be due to climate, geopolitical, or financial considerations.",
                 style = "font-size: 25px; text-align: center; margin-botton: 15px:"
               ),
-              
+
                 ),
-              
+
                 #Diverging Bar Graph Panel
                 tabPanel("Percent Variation in Production",
-                  
+
               sliderInput("Years", "Years of Production:",
                 min = 1990,
                 max = 2022,
@@ -162,33 +162,33 @@ ui <- fluidPage(theme = shinytheme("cosmo"),
                 sep = ""
                         ),
               hr(),
-                
+
               plotOutput("PercentChangevsProduct", width = 800, height = 600),
                 )
             )
-               
+
           )
-             
+
         )
-      
+
     ),
-  
-  #WORLD EVENTS AFFECT ON AGRICULTURE  
-  
+
+  #WORLD EVENTS AFFECT ON AGRICULTURE
+
   tabPanel("World Events",
-    titlePanel("How do Major Events in a Country Affect the Agriculture Production for that Country?"), 
-    
-    
+    titlePanel("How do Major Events in a Country Affect the Agriculture Production for that Country?"),
+
+
     div(
-    
+
       p(
         "In this tab, we explore the impact of significant world events on agricultural production to provide a different perspective on factors influencing agriculture other than climate change. While climate change is an importaant issue for agriculture, other events - such as natural disasters, wars, political uprisings, and epidemics. These events can affect infrastructure and hinder the production of crops. It is important to note that these charts are in the context of one country. Also, focus on the year of the event and the year following the event.",
         style = "font-size: 25px; text-align: center; margin-bottom: 45px; margin-top: 45px;"
       )
     ),
-  
-    
-    
+
+
+
     sidebarLayout(
       sidebarPanel(
         selectInput(
@@ -202,62 +202,62 @@ ui <- fluidPage(theme = shinytheme("cosmo"),
             "Arab Spring (2011)",
             "Russia Annexation of Crimea (2014)",
             "Ebola Outbreak in West Africa (2014)"
-          ), 
-          
-          
+          ),
+
+
           selected = "1994 Rwandan Civil War"
         )
       ),
-      
-      
-      #displays scatterplot 
+
+
+      #displays scatterplot
       mainPanel(
         plotOutput("Yield_Plot", height = "600px"),
-        
+
         p(
           "The Seven Events Explored:",
           style = "font-size: 20px; text-align: left; margin-bottom: 30px;"
         ),
-        
+
         p(
           "1. The Great Flood of 1993: The Mississippi River flooded and it affected numerous states in the USA, including North Dakota, South Dakota, Nebraska, Kansas, Missouri, Wisconsin, and Illinois.",
           style = "font-size: 15px; text-align: left; margin-bottom: 20px;"
         ),
-        
+
         p(
           "2. The Rwandan Civil War (1994): A violent conflict in Rwanda that resulted in the loss of over 800,000 lives within approximately 100 days.",
           style = "font-size: 15px; text-align: left; margin-bottom: 20px;"
         ),
-        
+
         p(
           "3. The Iraq War (2003): The United States invaded Iraq to overthrow Saddam Hussein's regime, leading to prolonged violence.",
           style = "font-size: 15px; text-align: left; margin-bottom: 20px;"
         ),
-        
+
         p(
           "4. The Indian Ocean Tsunami (2004): A massive undersea earthquake, causing widespread destruction and loss of life across several countries in Southeast Asia, especially Indonesia.",
           style = "font-size: 15px; text-align: left; margin-bottom: 20px;"
-        ),     
-        
+        ),
+
         p(
           "5. Arab Spring (2011): A series of pro-democracy uprisings and protests across the Middle East and North Africa (we are focusing on the impact of agricultural production in Egypt).",
           style = "font-size: 15px; text-align: left; margin-bottom: 20px;"
-        ), 
-        
+        ),
+
         p(
           "6. The Russia Annexation of Crimea (2014): A controversial takeover in which Russia seized control of the Crimean Peninsula from Ukraine.",
           style = "font-size: 15px; text-align: left; margin-bottom: 20px;"
-        ),       
-        
+        ),
+
         p(
           "7. The Ebola Outbreak in West Africa (2014): A severe public health crisis that highlighted the need for global health preparedness.",
           style = "font-size: 15px; text-align: left;"
-        )      
-        
+        )
+
           )
         )
-    
+
       )
-  
+
   )
 )
